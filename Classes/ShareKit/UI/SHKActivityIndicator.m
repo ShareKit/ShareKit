e2587fc6d5ab2ff234584dfe10f258aea213783e
Simon Maddox <simon@simonmaddox.com>
2011-02-08 14:18:24 +0000

    Fixed issue where Activity Indicator wouldnt have the correct orientation if the app was launched in Landscape, and orientation wasnt changed until showing the indicator

--- Classes/ShareKit/UI/SHKActivityIndicator.m
+++ Classes/ShareKit/UI/SHKActivityIndicator.m
@@ -252,7 +252,7 @@ static SHKActivityIndicator *currentIndicator = nil;
 
 - (void)setProperRotation:(BOOL)animated
 {
-	UIDeviceOrientation orientation = [[UIDevice currentDevice] orientation];
+	UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
 	
 	if (animated)
 	{
@@ -260,21 +260,22 @@ static SHKActivityIndicator *currentIndicator = nil;
 		[UIView setAnimationDuration:0.3];
 	}
 	
-	if (orientation == UIDeviceOrientationPortraitUpsideDown)
+	if (orientation == UIInterfaceOrientationPortraitUpsideDown)
 		self.transform = CGAffineTransformRotate(CGAffineTransformIdentity, SHKdegreesToRadians(180));	
-		
-	else if (orientation == UIDeviceOrientationPortrait)
+	
+	else if (orientation == UIInterfaceOrientationPortrait)
 		self.transform = CGAffineTransformRotate(CGAffineTransformIdentity, SHKdegreesToRadians(0)); 
 	
-	else if (orientation == UIDeviceOrientationLandscapeLeft)
+	else if (orientation == UIInterfaceOrientationLandscapeRight)
 		self.transform = CGAffineTransformRotate(CGAffineTransformIdentity, SHKdegreesToRadians(90));	
 	
-	else if (orientation == UIDeviceOrientationLandscapeRight)
+	else if (orientation == UIInterfaceOrientationLandscapeLeft)
 		self.transform = CGAffineTransformRotate(CGAffineTransformIdentity, SHKdegreesToRadians(-90));
 	
 	if (animated)
 		[UIView commitAnimations];
 }
+
 
 
 @end
