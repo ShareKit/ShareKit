c4beb19cafbad4b55949c0a51b38833f09697042
Vilém Kurz <vilem.kurz@cocoaminers.com>
2011-11-22 14:10:28 +0100

    SHAREKIT DEMO: added ShareKit demo app facebookAppID + slightly updated info about SHKFacebookLocalAppID

--- Classes/ShareKit/Configuration/DefaultSHKConfigurator.m
+++ Classes/ShareKit/Configuration/DefaultSHKConfigurator.m
@@ -69,13 +69,14 @@
 }
 // Facebook - https://developers.facebook.com/apps
 // SHKFacebookAppID is the Application ID provided by Facebook
-// SHKFacebookLocalAppID is used if you need to differentiate between several iOS apps running against a single Facebook app. Leave it blank unless you are sure of what you are doing. 
+// SHKFacebookLocalAppID is used if you need to differentiate between several iOS apps running against a single Facebook app. Useful, if you have full and lite versions of the same app,
+// and wish sharing from both will appear on facebook as sharing from one main app. You have to add different suffix to each version. Do not forget to fill both suffixes on facebook developer ("URL Scheme Suffix"). Leave it blank unless you are sure of what you are doing. 
 // The CFBundleURLSchemes in your App-Info.plist should be "fb" + the concatenation of these two IDs.
 // Example: 
 //    SHKFacebookAppID = 555
-//    SHKFacebookLocalAppID = funk
+//    SHKFacebookLocalAppID = lite
 // 
-//    Your CFBundleURLSchemes entry: fb555funk
+//    Your CFBundleURLSchemes entry: fb555lite
 - (NSString*)facebookAppId {
 	return @"";
 }
