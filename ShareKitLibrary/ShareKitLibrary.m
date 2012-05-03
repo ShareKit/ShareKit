7aa43b10be77b39e01a862306522933675a29078
Vilém Kurz <vilem.kurz@cocoaminers.com>
2012-05-03 16:04:34 +0200

    NEW FEATURE: added ShareKitLibrary target

---
+++ ShareKitLibrary/ShareKitLibrary.m
@@ -0,0 +1,13 @@
+//
+//  ShareKitLibrary.m
+//  ShareKitLibrary
+//
+//  Created by Vilem Kurz on 03/05/2012.
+//  Copyright (c) 2012 Cocoa Miners. All rights reserved.
+//
+
+#import "ShareKitLibrary.h"
+
+@implementation ShareKitLibrary
+
+@end
