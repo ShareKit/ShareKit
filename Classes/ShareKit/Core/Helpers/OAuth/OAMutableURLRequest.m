805ed944cbd1837a200175a3664be9eab4880629
Berik Visschers <berik.visschers@xaton.com>
2012-07-19 15:29:34 +0200

    Fix nil variable problem

--- Classes/ShareKit/Core/Helpers/OAuth/OAMutableURLRequest.m
+++ Classes/ShareKit/Core/Helpers/OAuth/OAMutableURLRequest.m
@@ -159,7 +159,7 @@ signatureProvider:(id<OASignatureProviding, NSObject>)aProvider
     
     // set OAuth headers
     NSString *oauthToken;
-    if ([token.key isEqualToString:@""])
+    if (token.key.length == 0)
         oauthToken = @""; // not used on Request Token transactions
     else
         oauthToken = [NSString stringWithFormat:@"oauth_token=\"%@\", ", [token.key URLEncodedString]];
@@ -218,7 +218,7 @@ signatureProvider:(id<OASignatureProviding, NSObject>)aProvider
 	[parameterPairs addObject:[[OARequestParameter requestParameterWithName:@"oauth_nonce" value:nonce] URLEncodedNameValuePair]];
 	[parameterPairs addObject:[[OARequestParameter requestParameterWithName:@"oauth_version" value:@"1.0"] URLEncodedNameValuePair]];
     
-    if (![token.key isEqualToString:@""]) {
+    if (token.key.length > 0) {
         [parameterPairs addObject:[[OARequestParameter requestParameterWithName:@"oauth_token" value:token.key] URLEncodedNameValuePair]];
     }
     
