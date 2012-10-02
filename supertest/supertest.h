728e3154c23b05b36d47f693b755514bdd3546cc
SubMarine <vnikitin@mistralservice.ru>
2012-10-02 23:01:38 +0400

    Dropbox SDK integration

---
+++ supertest/supertest.h
@@ -0,0 +1,13 @@
+//
+//  supertest.h
+//  supertest
+//
+//  Created by submarine on 10/2/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface supertest : NSObject
+
+@end
---
+++ supertest/supertest.m
@@ -0,0 +1,13 @@
+//
+//  supertest.m
+//  supertest
+//
+//  Created by submarine on 10/2/12.
+//
+//
+
+#import "supertest.h"
+
+@implementation supertest
+
+@end
