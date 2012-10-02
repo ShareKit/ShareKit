0b991ec187be9077ed59285a18d16333505d98c5
James Chen <ashchan@gmail.com>
2012-10-02 22:53:42 +0900

    added service for Sina Weibo

---
+++ Classes/ShareKit/Sharers/Services/Sina Weibo/SHKSinaWeibo.h
@@ -0,0 +1,16 @@
+//
+//  SHKSinaWeibo.h
+//  ShareKit
+//
+//  Created by James Chen on 10/2/2012.
+//  Copyright 2012 ashchan.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "SHKSharer.h"
+
+@interface SHKSinaWeibo : SHKSharer {
+
+}
+
+@end
