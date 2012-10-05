991fbfd25001f3677302af7d7605e07e041ee38a
SubMarine <vnikitin@mistralservice.ru>
2012-10-05 11:55:33 +0400

    improvements in libDropboxSDK libDropbox created

---
+++ Dropbox/Dropbox.h
@@ -0,0 +1,13 @@
+//
+//  Dropbox.h
+//  Dropbox
+//
+//  Created by submarine on 10/5/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Dropbox : NSObject
+
+@end
---
+++ Dropbox/Dropbox.m
@@ -0,0 +1,13 @@
+//
+//  Dropbox.m
+//  Dropbox
+//
+//  Created by submarine on 10/5/12.
+//
+//
+
+#import "Dropbox.h"
+
+@implementation Dropbox
+
+@end
