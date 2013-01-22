ea2cfb832b1f314881edde5ffa4074c5d8186efc
Christopher White <mrchristopher124@gmail.com>
2013-01-22 12:42:54 -0500

    add stub for accounts view controller

---
+++ Classes/ExampleAccountsViewController.m
@@ -0,0 +1,38 @@
+//
+//  ExampleAccountsViewController.m
+//  ShareKit
+//
+//  Created by Chris White on 1/22/13.
+//
+//
+
+#import "ExampleAccountsViewController.h"
+
+@interface ExampleAccountsViewController ()
+
+@end
+
+@implementation ExampleAccountsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
