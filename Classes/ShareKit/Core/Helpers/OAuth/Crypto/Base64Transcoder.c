34b3c4ecaaa00299d451301e75ba7fe680af5d6e
Jiqun Zheng <jzheng@ilegendsoft.com>
2013-01-23 14:20:58 +0800

    Rename Base64 functions to resolve the duplicate symbol problem

--- Classes/ShareKit/Core/Helpers/OAuth/Crypto/Base64Transcoder.c
+++ Classes/ShareKit/Core/Helpers/OAuth/Crypto/Base64Transcoder.c
@@ -30,7 +30,7 @@
 #include <math.h>
 #include <string.h>
 
-const u_int8_t kBase64EncodeTable[64] = {
+const u_int8_t kILSBase64EncodeTable[64] = {
 	/*  0 */ 'A',	/*  1 */ 'B',	/*  2 */ 'C',	/*  3 */ 'D', 
 	/*  4 */ 'E',	/*  5 */ 'F',	/*  6 */ 'G',	/*  7 */ 'H', 
 	/*  8 */ 'I',	/*  9 */ 'J',	/* 10 */ 'K',	/* 11 */ 'L', 
@@ -57,7 +57,7 @@ const u_int8_t kBase64EncodeTable[64] = {
 -5 = Illegal noise (null byte)
 */
 
-const int8_t kBase64DecodeTable[128] = {
+const int8_t kILSBase64DecodeTable[128] = {
 	/* 0x00 */ -5, 	/* 0x01 */ -3, 	/* 0x02 */ -3, 	/* 0x03 */ -3,
 	/* 0x04 */ -3, 	/* 0x05 */ -3, 	/* 0x06 */ -3, 	/* 0x07 */ -3,
 	/* 0x08 */ -3, 	/* 0x09 */ -2, 	/* 0x0a */ -2, 	/* 0x0b */ -2,
@@ -92,32 +92,32 @@ const int8_t kBase64DecodeTable[128] = {
 	/* '|' */ -3,	/* '}' */ -3,	/* '~' */ -3,	/* 0x7f */ -3
 };
 
-const u_int8_t kBits_00000011 = 0x03;
-const u_int8_t kBits_00001111 = 0x0F;
-const u_int8_t kBits_00110000 = 0x30;
-const u_int8_t kBits_00111100 = 0x3C;
-const u_int8_t kBits_00111111 = 0x3F;
-const u_int8_t kBits_11000000 = 0xC0;
-const u_int8_t kBits_11110000 = 0xF0;
-const u_int8_t kBits_11111100 = 0xFC;
+const u_int8_t kILSBits_00000011 = 0x03;
+const u_int8_t kILSBits_00001111 = 0x0F;
+const u_int8_t kILSBits_00110000 = 0x30;
+const u_int8_t kILSBits_00111100 = 0x3C;
+const u_int8_t kILSBits_00111111 = 0x3F;
+const u_int8_t kILSBits_11000000 = 0xC0;
+const u_int8_t kILSBits_11110000 = 0xF0;
+const u_int8_t kILSBits_11111100 = 0xFC;
 
-size_t EstimateBas64EncodedDataSize(size_t inDataSize)
+size_t ILSEstimateBas64EncodedDataSize(size_t inDataSize)
 {
 size_t theEncodedDataSize = (int)ceil(inDataSize / 3.0) * 4;
 theEncodedDataSize = theEncodedDataSize / 72 * 74 + theEncodedDataSize % 72;
 return(theEncodedDataSize);
 }
 
-size_t EstimateBas64DecodedDataSize(size_t inDataSize)
+size_t ILSEstimateBas64DecodedDataSize(size_t inDataSize)
 {
 size_t theDecodedDataSize = (int)ceil(inDataSize / 4.0) * 3;
 //theDecodedDataSize = theDecodedDataSize / 72 * 74 + theDecodedDataSize % 72;
 return(theDecodedDataSize);
 }
 
-bool Base64EncodeData(const void *inInputData, size_t inInputDataSize, char *outOutputData, size_t *ioOutputDataSize)
+bool ILSBase64EncodeData(const void *inInputData, size_t inInputDataSize, char *outOutputData, size_t *ioOutputDataSize)
 {
-size_t theEncodedDataSize = EstimateBas64EncodedDataSize(inInputDataSize);
+size_t theEncodedDataSize = ILSEstimateBas64EncodedDataSize(inInputDataSize);
 if (*ioOutputDataSize < theEncodedDataSize)
 	return(false);
 *ioOutputDataSize = theEncodedDataSize;
@@ -125,10 +125,10 @@ const u_int8_t *theInPtr = (const u_int8_t *)inInputData;
 u_int32_t theInIndex = 0, theOutIndex = 0;
 for (; theInIndex < (inInputDataSize / 3) * 3; theInIndex += 3)
 	{
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kBits_11110000) >> 4];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 1] & kBits_00001111) << 2 | (theInPtr[theInIndex + 2] & kBits_11000000) >> 6];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 2] & kBits_00111111) >> 0];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex] & kILSBits_11111100) >> 2];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex] & kILSBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kILSBits_11110000) >> 4];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex + 1] & kILSBits_00001111) << 2 | (theInPtr[theInIndex + 2] & kILSBits_11000000) >> 6];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex + 2] & kILSBits_00111111) >> 0];
 	if (theOutIndex % 74 == 72)
 		{
 		outOutputData[theOutIndex++] = '\r';
@@ -138,8 +138,8 @@ for (; theInIndex < (inInputDataSize / 3) * 3; theInIndex += 3)
 const size_t theRemainingBytes = inInputDataSize - theInIndex;
 if (theRemainingBytes == 1)
 	{
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (0 & kBits_11110000) >> 4];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex] & kILSBits_11111100) >> 2];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex] & kILSBits_00000011) << 4 | (0 & kILSBits_11110000) >> 4];
 	outOutputData[theOutIndex++] = '=';
 	outOutputData[theOutIndex++] = '=';
 	if (theOutIndex % 74 == 72)
@@ -150,9 +150,9 @@ if (theRemainingBytes == 1)
 	}
 else if (theRemainingBytes == 2)
 	{
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_11111100) >> 2];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex] & kBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kBits_11110000) >> 4];
-	outOutputData[theOutIndex++] = kBase64EncodeTable[(theInPtr[theInIndex + 1] & kBits_00001111) << 2 | (0 & kBits_11000000) >> 6];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex] & kILSBits_11111100) >> 2];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex] & kILSBits_00000011) << 4 | (theInPtr[theInIndex + 1] & kILSBits_11110000) >> 4];
+	outOutputData[theOutIndex++] = kILSBase64EncodeTable[(theInPtr[theInIndex + 1] & kILSBits_00001111) << 2 | (0 & kILSBits_11000000) >> 6];
 	outOutputData[theOutIndex++] = '=';
 	if (theOutIndex % 74 == 72)
 		{
@@ -163,11 +163,11 @@ else if (theRemainingBytes == 2)
 return(true);
 }
 
-bool Base64DecodeData(const void *inInputData, size_t inInputDataSize, void *ioOutputData, size_t *ioOutputDataSize)
+bool ILSBase64DecodeData(const void *inInputData, size_t inInputDataSize, void *ioOutputData, size_t *ioOutputDataSize)
 {
 memset(ioOutputData, '.', *ioOutputDataSize);
 
-size_t theDecodedDataSize = EstimateBas64DecodedDataSize(inInputDataSize);
+size_t theDecodedDataSize = ILSEstimateBas64DecodedDataSize(inInputDataSize);
 if (*ioOutputDataSize < theDecodedDataSize)
 	return(false);
 *ioOutputDataSize = 0;
@@ -181,44 +181,44 @@ for (; theInIndex < inInputDataSize; )
 	int8_t theSextet = 0;
 	
 	int8_t theCurrentInputOctet = theInPtr[theInIndex];
-	theSextet = kBase64DecodeTable[theCurrentInputOctet];
+	theSextet = kILSBase64DecodeTable[theCurrentInputOctet];
 	if (theSextet == -1)
 		break;
 	while (theSextet == -2)
 		{
 		theCurrentInputOctet = theInPtr[++theInIndex];
-		theSextet = kBase64DecodeTable[theCurrentInputOctet];
+		theSextet = kILSBase64DecodeTable[theCurrentInputOctet];
 		}
 	while (theSextet == -3)
 		{
 		theCurrentInputOctet = theInPtr[++theInIndex];
-		theSextet = kBase64DecodeTable[theCurrentInputOctet];
+		theSextet = kILSBase64DecodeTable[theCurrentInputOctet];
 		}
 	if (theSequence == 0)
 		{
-		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 2 & kBits_11111100;
+		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 2 & kILSBits_11111100;
 		}
 	else if (theSequence == 1)
 		{
-		theOutputOctet |= (theSextet >- 0 ? theSextet : 0) >> 4 & kBits_00000011;
+		theOutputOctet |= (theSextet >- 0 ? theSextet : 0) >> 4 & kILSBits_00000011;
 		theOutPtr[theOutIndex++] = theOutputOctet;
 		}
 	else if (theSequence == 2)
 		{
-		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 4 & kBits_11110000;
+		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 4 & kILSBits_11110000;
 		}
 	else if (theSequence == 3)
 		{
-		theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 2 & kBits_00001111;
+		theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 2 & kILSBits_00001111;
 		theOutPtr[theOutIndex++] = theOutputOctet;
 		}
 	else if (theSequence == 4)
 		{
-		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 6 & kBits_11000000;
+		theOutputOctet = (theSextet >= 0 ? theSextet : 0) << 6 & kILSBits_11000000;
 		}
 	else if (theSequence == 5)
 		{
-		theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 0 & kBits_00111111;
+		theOutputOctet |= (theSextet >= 0 ? theSextet : 0) >> 0 & kILSBits_00111111;
 		theOutPtr[theOutIndex++] = theOutputOctet;
 		}
 	theSequence = (theSequence + 1) % 6;
