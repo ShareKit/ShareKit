24c71190ac50a4a71d641669b9f06944a765166f
Jiahuan Qin <jqin@ilegendsoft.com>
2013-02-01 17:10:42 +0800

    add   framework  target

---
+++ ILSShareKitTests/ILSShareKitTests.h
@@ -0,0 +1,13 @@
+//
+//  ILSShareKitTests.h
+//  ILSShareKitTests
+//
+//  Created by jiahuan on 2/1/13.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface ILSShareKitTests : SenTestCase
+
+@end
---
+++ ILSShareKitTests/ILSShareKitTests.m
@@ -0,0 +1,32 @@
+//
+//  ILSShareKitTests.m
+//  ILSShareKitTests
+//
+//  Created by jiahuan on 2/1/13.
+//
+//
+
+#import "ILSShareKitTests.h"
+
+@implementation ILSShareKitTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in ILSShareKitTests");
+}
+
+@end
