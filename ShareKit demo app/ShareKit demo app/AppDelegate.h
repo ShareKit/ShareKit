279ad3d840417cae1c181edf15c687c47b8830f8
Vilém Kurz <vilem.kurz@cocoaminers.com>
2013-05-13 11:56:33 +0200

    REFACTORING: ShareKit demo app project separated from library. The purpose is to more closely simulate real world ShareKit usage.

---
+++ ShareKit demo app/ShareKit demo app/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ShareKit demo app
+//
+//  Created by Vilem Kurz on 13/05/2013.
+//  Copyright (c) 2013 Vilem Kurz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
