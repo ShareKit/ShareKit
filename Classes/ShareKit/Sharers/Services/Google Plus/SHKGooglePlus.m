5fed3dd393d2bf9dad7122cb449bd6d5e9e33d68
Vilém Kurz <vilem.kurz@cocoaminers.com>
2013-12-11 16:56:11 +0100

    NEW FEATURE: google+ image and video share

--- Classes/ShareKit/Sharers/Services/Google Plus/SHKGooglePlus.m
+++ Classes/ShareKit/Sharers/Services/Google Plus/SHKGooglePlus.m
@@ -13,6 +13,9 @@
 #import <GooglePlus/GPPSignIn.h>
 #import "GTLPlusPerson.h"
 
+#define ALLOWED_VIDEO_SIZE 1037741824 //1GB in Bytes
+#define ALLOWED_IMAGE_SIZE 37748736 //36MB in Bytes
+
 @interface SHKGooglePlus ()
 
 @property BOOL isDisconnecting;
@@ -28,6 +31,17 @@
 
 + (BOOL)canShareURL { return YES; }
 + (BOOL)canShareText { return YES; }
++ (BOOL)canShareImage { return YES; }
++ (BOOL)canShareFile:(SHKFile *)file {
+    
+    BOOL isAllowedVideo = [file.mimeType hasPrefix:@"video"] && file.size < ALLOWED_VIDEO_SIZE;
+    BOOL isAllowedImage = [file.mimeType hasPrefix:@"image"] && file.size < ALLOWED_IMAGE_SIZE;
+    if (isAllowedVideo || isAllowedImage) {
+        return YES;
+    } else {
+        return NO;
+    }
+}
 
 + (BOOL)canShareOffline { return NO; }
 + (BOOL)canAutoShare { return NO; }
@@ -58,6 +72,9 @@
         [[GPPSignIn sharedInstance] setDelegate:self];
         result = [[GPPSignIn sharedInstance] trySilentAuthentication];
         [[SHK currentHelper] keepSharerReference:self];
+        
+        //will be shared in auth callback. Without this Google+ native share sheet says: "User must be signed in to use the native sharebox." Here we can get if user has authorized before, but the app was killed/hibernated in the meantime
+        if (self.item) self.pendingAction = SHKPendingShare;
     }
     
 	return result;
@@ -130,14 +147,26 @@
     [[GPPShare sharedInstance] setDelegate:self];
     id<GPPShareBuilder> mShareBuilder = [[GPPShare sharedInstance] nativeShareDialog];
     
+    [mShareBuilder setPrefillText:self.item.text];
+    
     switch ([self.item shareType]) {
         case SHKShareTypeURL:
             [mShareBuilder setURLToShare:self.item.URL];
-            [mShareBuilder setPrefillText:self.item.text];
+            break;
+        case SHKShareTypeText:
+            break;
+        case SHKShareTypeImage:
+            [(id<GPPNativeShareBuilder>)mShareBuilder attachImage:self.item.image];
+            break;
+        case SHKShareTypeFile:
+            if ([self.item.file.mimeType hasPrefix:@"image"]) {
+                [(id<GPPNativeShareBuilder>)mShareBuilder attachImageData:[self.item.file data]];
+            } else { //video
+                [(id<GPPNativeShareBuilder>)mShareBuilder attachVideoURL:self.item.file.URL];
+            }
             break;
         default:
-        case SHKShareTypeText:
-            [mShareBuilder setPrefillText:self.item.text];
+            return NO;
             break;
     }
     self.quiet = YES; //if user cancels, on return blinks activity indicator. This disables it, as we share in safari and it is hidden anyway
