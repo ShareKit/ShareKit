ShareKit/ShareKit

commit 80c75af751c2391e79ef0172ebd77e653f9bda41

BUGFIX: SHKTextMessage now check, if file is allowed

Classes/ShareKit/Core/Helpers/SHKFile.m
@@ -54,7 +54,8 @@ static NSString *tempDirectory;
         
         _path = path;
         _filename = path.lastPathComponent;
-        _mimeType = [self MIMETypeForPath:self.filename];
+        _mimeType = [self MIMETypeForPath:_filename];
+        _UTIType = [self NSStringUTITypeForPath:_filename];
     }
     return self;
 }
@@ -70,7 +71,8 @@ static NSString *tempDirectory;
         if (!filename) filename = [NSString stringWithFormat:@"ShareKit_file_%li", random() % 100];
         
         _filename = filename;
-        _mimeType = [self MIMETypeForPath:self.filename];
+        _mimeType = [self MIMETypeForPath:filename];
+        _UTIType = [self NSStringUTITypeForPath:filename];
     }
     return self;
 }
@@ -87,12 +89,14 @@ static NSString *tempDirectory;
             
             _filename = _path.lastPathComponent;
             _mimeType = [self MIMETypeForPath:_filename];
+            _UTIType = [self NSStringUTITypeForPath:_filename];
         
         } else {
             
             _data = [decoder decodeObjectForKey:kSHKFileData];
             _filename = [decoder decodeObjectForKey:kSHKFileName];
             _mimeType = [self MIMETypeForPath:_filename];
+            _UTIType = [self NSStringUTITypeForPath:_filename];
         }
     }
     return self;
@@ -229,14 +233,32 @@ static NSString *kSHKFileData = @"kSHKFileData";
 #pragma mark Utility
 
 - (NSString *)MIMETypeForPath:(NSString *)path{
+    
     NSString *result = @"";
-    NSString *extension = [path pathExtension];
-    CFStringRef uti = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)extension, NULL);
+    CFStringRef uti = [self UTITypeForPath:path];
     if (uti) {
         CFStringRef cfMIMEType = UTTypeCopyPreferredTagWithClass(uti, kUTTagClassMIMEType);
         if (cfMIMEType) {
             result = CFBridgingRelease(cfMIMEType);
         }
+        CFRelease(uti);
+    }
+    return result;
+}
+
+- (CFStringRef)UTITypeForPath:(NSString *)path {
+    
+    NSString *extension = [path pathExtension];
+    CFStringRef result = UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)extension, NULL);
+    return result;
+}
+
+- (NSString *)NSStringUTITypeForPath:(NSString *)path {
+    
+    NSString *result = nil;
+    CFStringRef uti = [self UTITypeForPath:path];
+    if (uti) {
+        result = CFBridgingRelease(uti);
         CFRelease(uti);
     }
     return result;

Classes/ShareKit/Core/Helpers/SHKFile.h
@@ -48,7 +48,11 @@
     Derived from filename. Used by services to check, if they can handle the file. Also favorites are different for each mime type.
  */
 @property (nonatomic,readonly) NSString *mimeType;
-/*! 
+/*!
+ Derived from filename. Used by services to check, if they can handle the file.
+ */
+@property (nonatomic,readonly) NSString *UTIType;
+/*!
     Size in bytes
  */
 @property (nonatomic,readonly) NSUInteger size;

Classes/ShareKit/Sharers/Actions/Text Message/SHKTextMessage.h
@@ -30,9 +30,6 @@
 #import "SHKSharer.h"
 
 @interface SHKTextMessage : SHKSharer <MFMessageComposeViewControllerDelegate>
-{
-	
-}
 
 - (BOOL)sendText;
 

Classes/ShareKit/Sharers/Actions/Text Message/SHKTextMessage.m
@@ -32,9 +32,10 @@
 
 #pragma mark -
 #pragma mark Configuration : Service Defination
-+(BOOL)composerSupportsAttachment
+
++ (BOOL)composerSupportsAttachment
 {
-	return [[MFMessageComposeViewController class] respondsToSelector:@selector(canSendAttachments) ];
+	return [[MFMessageComposeViewController class] respondsToSelector:@selector(canSendAttachments)];
 }
 
 + (NSString *)sharerTitle
@@ -58,8 +59,9 @@
 }
 
 + (BOOL)canShareFile:(SHKFile *)file {
-    if([self composerSupportsAttachment] && [MFMessageComposeViewController canSendAttachments]){
-		// there is probably some number that we should limit attachments to, for the moment just say ok.
+    
+    if ([self composerSupportsAttachment] && [MFMessageComposeViewController canSendAttachments] && [MFMessageComposeViewController isSupportedAttachmentUTI:file.UTIType]) {
+
 		return YES;
 	}
 	return NO;
@@ -75,7 +77,6 @@
 	return NO;
 }
 
-
 #pragma mark -
 #pragma mark Configuration : Dynamic Enable
 
@@ -83,13 +84,6 @@
 {
 	return [MFMessageComposeViewController canSendText];
 }
-
-- (BOOL)shouldAutoShare
-{
-	return YES;
-}
-
-
 
 #pragma mark -
 #pragma mark Share API Methods
@@ -169,6 +163,5 @@
     [[SHK currentHelper] hideCurrentViewControllerAnimated:YES];
     [[SHK currentHelper] removeSharerReference:self]; //retained in [self sendText] method
 }
-
 
 @end

commit 3ad6dec8f4f172303cc58dc47493abec8a4332c4

BUGFIX: fixed crash with double releasing CFStringRef, introduced in 80c75af751c2391e79ef0172ebd77e653f9bda41

Classes/ShareKit/Core/Helpers/SHKFile.m
@@ -259,7 +259,6 @@ static NSString *kSHKFileData = @"kSHKFileData";
     CFStringRef uti = [self UTITypeForPath:path];
     if (uti) {
         result = CFBridgingRelease(uti);
-        CFRelease(uti);
     }
     return result;
 }