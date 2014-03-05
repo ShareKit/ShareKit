56a30a5e1a71c120e20f227801e5dc34fbc015dc
Vilém Kurz <vilem.kurz@cocoaminers.com>
2014-03-05 13:11:12 +0100

    NEW FEATURE: SHKiOSFacebook can report progress with video uploads. Also if item hasPath, it is streamed instead read into memory

--- Classes/ShareKit/Core/Helpers/SHKFile.m
+++ Classes/ShareKit/Core/Helpers/SHKFile.m
@@ -169,9 +169,13 @@ static NSString *kSHKFileData = @"kSHKFileData";
     _path = [tempDirectory stringByAppendingPathComponent:sanitizedFileName];
     
     // Create our file
-    if([[NSFileManager defaultManager] fileExistsAtPath:_path]) {
+    if([[NSFileManager defaultManager] fileExistsAtPath:_path]){
+        
         // TODO: This file already exists - throw an error
-        NSAssert(NO, @"file already exists?!");
+        
+        //SHKLog(@"file already exists, OK?");
+        //return;
+        //NSAssert(NO, @"file already exists?!");
     }
     
     // Read our file into the file system
--- Classes/ShareKit/Sharers/Services/Facebook/SHKiOSFacebook.m
+++ Classes/ShareKit/Sharers/Services/Facebook/SHKiOSFacebook.m
@@ -11,9 +11,14 @@
 
 #import "SharersCommonHeaders.h"
 #import "SHKFacebookCommon.h"
+#import "SHKSession.h"
+
 #import "NSMutableDictionary+NSNullsToEmptyStrings.h"
+#import "NSMutableURLRequest+Parameters.h"
 
 #import <Accounts/Accounts.h>
+
+typedef void (^SHKRequestHandler)(NSData *responseData, NSURLResponse *urlResponse, NSError *error);
 
 @implementation SHKiOSFacebook
 
@@ -191,9 +196,17 @@
                                             requestMethod:SLRequestMethodPOST
                                                       URL:[NSURL URLWithString:kSHKFacebookAPIVideosURL]
                                                parameters:params];
-    [request addMultipartData:self.item.file.data withName:@"source" type:self.item.file.mimeType filename:self.item.file.filename];
     request.account = [self availableAccounts][0];
-    [request performRequestWithHandler:[self requestHandler]];
+    
+    BOOL canUseNSURLSession = NSClassFromString(@"NSURLSession") != nil;
+    if (canUseNSURLSession) {
+        NSURLRequest *preparedRequest = [request preparedURLRequest];
+        [(NSMutableURLRequest *)preparedRequest attachFile:self.item.file withParameterName:@"source"];
+        self.networkSession = [SHKSession startSessionWithRequest:preparedRequest delegate:self completion:[self requestHandler]];
+    } else {
+        [request addMultipartData:self.item.file.data withName:@"source" type:self.item.file.mimeType filename:self.item.file.filename];
+        [request performRequestWithHandler:[self requestHandler]];
+    }
     
     //update video limits
     [[self class] getUserInfo];
@@ -218,9 +231,9 @@
     [videoLimitsRequest performRequestWithHandler:[self requestHandler]];
 }
 
-- (SLRequestHandler)requestHandler {
+- (SHKRequestHandler)requestHandler {
     
-    SLRequestHandler result = ^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+    SHKRequestHandler result = ^(NSData *responseData, NSURLResponse *urlResponse, NSError *error) {
         
         [[NSOperationQueue mainQueue] addOperationWithBlock:^{
             
@@ -229,7 +242,7 @@
                 NSError *parseError;
                 NSMutableDictionary *parsedResponse = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:&parseError];
                 
-                BOOL requestSucceeded = urlResponse.statusCode < 400;
+                BOOL requestSucceeded = [(NSHTTPURLResponse *)urlResponse statusCode] < 400;
                 if (requestSucceeded) {
                     
                     //if this is userinfo, save it
@@ -254,7 +267,7 @@
                     NSUInteger errorCode = [parsedResponse[@"error"][@"code"] integerValue];
                     
                     //even for 458 (user removed app on Facebook) we should refresh token - this way iOS settings app removes access too. Then we can reauthorize again, if user shares.
-                    if (errorSubCode == 458 || errorSubCode == 463 || errorSubCode == 467 || errorCode == 2500 || 200 <= errorCode || errorCode <= 299) {
+                    if (errorSubCode == 458 || errorSubCode == 463 || errorSubCode == 467 || errorCode == 2500 || 200 >= errorCode || errorCode <= 299 || errorCode == 102) {
                         [self shouldReloginWithPendingAction:SHKPendingRefreshToken];
                     } else {
                         [self sendShowSimpleErrorAlert];
