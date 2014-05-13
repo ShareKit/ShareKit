94cf5adf64e6924ecfbf4fed00346c8726824b1b
Vilém Kurz <vilem.kurz@cocoaminers.com>
2014-05-13 21:41:12 +0200

    BUGFIX: Doc change - Facebook app id is required also in iOS sharer. Fixes #914

--- Classes/ShareKit/Configuration/DefaultSHKConfigurator.m
+++ Classes/ShareKit/Configuration/DefaultSHKConfigurator.m
@@ -96,12 +96,9 @@ on the auth path. It will try to use native auth if availible.
      */
 }
 
-
-/* Bellow 2 conf items (appId and localAppId are needed only if you return YES from forcePreIOS6FacebookPosting */
-
 // Facebook - https://developers.facebook.com/apps
-// SHKFacebookAppID is the Application ID provided by Facebook
-// SHKFacebookLocalAppID is used if you need to differentiate between several iOS apps running against a single Facebook app. Useful, if you have full and lite versions of the same app,
+// facebookAppID is the Application ID provided by Facebook
+// facebookLocalAppID is used if you need to differentiate between several iOS apps running against a single Facebook app. Useful, if you have full and lite versions of the same app,
 // and wish sharing from both will appear on facebook as sharing from one main app. You have to add different suffix to each version. Do not forget to fill both suffixes on facebook developer ("URL Scheme Suffix"). Leave it blank unless you are sure of what you are doing. 
 // The CFBundleURLSchemes in your App-Info.plist should be "fb" + the concatenation of these two IDs.
 // Example: 
