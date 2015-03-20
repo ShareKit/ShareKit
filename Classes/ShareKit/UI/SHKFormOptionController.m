c945ad5853764a7c48ef23e721ff92d1c43d6780
Vilém Kurz <vilem.kurz@cocoaminers.com>
2015-03-20 13:04:30 +0100

    BUGFIX: fixed static analyser warning

--- Classes/ShareKit/UI/SHKFormOptionController.m
+++ Classes/ShareKit/UI/SHKFormOptionController.m
@@ -219,6 +219,8 @@
 }
 
 - (void)viewDidUnload {
+    
+    [super viewDidLoad];
     // Relinquish ownership of anything that can be recreated in viewDidLoad or on demand.
     // For example: self.myOutlet = nil;
 }
